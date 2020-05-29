cdb97fdb68603548a9f709fb0c929370bbcfe231
Caroline Horsch <chorsch@terpmail.umd.edu>
2020-05-29 03:07:01 -0400

    Updated solver to work with 3 ODEs for x, v, and L
    -(empty) ODEs for dx/dt, dv/dt. and dL/dt defined in Diffeqs calss.
    -(empty) equations for torque and force also in Diffeqs class

--- RK4Solver/DiffEqs.py
+++ RK4Solver/DiffEqs.py
@@ -1,7 +1,29 @@
 import numpy as np
 
-def dydt_grav(t, y, i, args):
-    v0, a = args
-    v0 = v0[i]
-    a = a[i]
+#sample diffeq used for testing purposes
+def dydt_grav(t, y, agent, args):
+    v0 = args['v0']
+    a = args['a']
     return np.array([v0[j] + a[j] * t for j in range(0,3)])
+
+#position
+def dxdt(t, x, agent, args):
+    return args['v']
+
+#velocity
+def dvdt(t, v, agent, args):
+    F = args['F'](v)
+    return F/args['m']
+
+#angular momentum
+def dLdt(t, L, agent, args):
+    T = args['T'](L)
+    return T - np.cross(args['w'], L)
+
+#torque
+def T(L):
+    return np.array([0,0,0])
+
+#force
+def F(v):
+    return np.array([0,0,0])
--- RK4Solver/solver.py
+++ RK4Solver/solver.py
@@ -7,59 +7,78 @@ class Solver():
     def __init__(self,
                  initial_vals = None,
                  ddt_args = None,
-                 diffeqs = None,
                  N = 1
                  ):
         self.N = N
+        self.agents = range(N)
         self.dims = ['x', 'y', 'z']
-        #self.vars = ['x', 'v', 'L']
-        self.vars = ['x']
-        self.agents = ["agent_{}".format(i) for i in range(N)]
+        self.vars = ['x', 'v', 'L']
+
+        # maps variable -> diffeq
+        self.diffeqs = {'x': de.dydt_grav, 'v': de.dvdt, 'L': de.dLdt}
+
+        # maps agent -> variable -> arguments
+        if ddt_args is None:
+            ddt_args = {{{} for var in self.vars} for agent in self.agents}
+        self.ddt_args = ddt_args
+
+        #maps agent -> variable -> current value
         if initial_vals is None:
             initial_vals = {agent:{var: [0.0 for dim in self.dims] for var in self.vars} for agent in self.agents}
         self.vals = initial_vals
 
 
-    def one_bird_update(self, ddt, dydt_args, t0, y, h, i):
-        k1 = h * ddt(t0, y, i, dydt_args)
-        k2 = h * ddt(t0 + 0.5 * h, y + 0.5 * k1, i, dydt_args)
-        k3 = h * ddt(t0 + 0.5 * h, y + 0.5 * k2, i, dydt_args)
-        k4 = h * ddt(t0 + h, y + k3, i, dydt_args)
+    def one_bird_update(self, ddt, t0, y, h, args, agent):
+        k1 = h * ddt(t0, y, agent, args)
+        k2 = h * ddt(t0 + 0.5 * h, y + 0.5 * k1, agent, args)
+        k3 = h * ddt(t0 + 0.5 * h, y + 0.5 * k2, agent, args)
+        k4 = h * ddt(t0 + h, y + k3, agent, args)
 
         return y + (1.0 / 6.0)*(k1 + (2 * k2) + (2 * k3) + k4)
 
-    # Finds value of y at a given t using step size h and initial value y0 at t0.
-    def solve(self, dydt, dydt_args, t0, t, h):
+
+    def solve(self, t0, t, h):
         n = (int)((t - t0)/h)
-        #Y holds the y values for every bird
-        Y = {agent: [] for agent in self.agents}
+
+        #Y holds the vals for each agent, varible -> agent -> values
+        Y = {var:{agent:[] for agent in self.agents} for var in self.vars}
         T = np.arange(0, t, h)
-        #go through all timesteps
-        for _ in range(1, n + 1):
-            #update each agent
+
+        for _ in range(n):
             for agent in self.agents:
                 for var in self.vars:
+
                     y = self.vals[agent][var]
-                    a = self.one_bird_update(dydt, dydt_args, t0, y, h, agent)
-                    self.vals[agent][var] = a
-                    Y[agent].append(np.copy(a))
+                    args = self.ddt_args[agent][var]
+                    ddt = self.diffeqs[var]
+
+                    #perform one timestep update to y
+                    step = self.one_bird_update(ddt, t0, y, h, args, agent)
+                    self.vals[agent][var] = step
+                    #store the values after making a single timestep
+                    Y[var][agent].append(np.copy(step))
+
             t0 = t0 + h
 
-        plot(T, Y, self.agents)
+        #currently only plots x
+        plot(T, Y['x'], self.agents)
         return Y
 
 
 # Running the solver
+# Right now this just solves gravity for position, with ODEs that do nothing for v and L. 
 if __name__ == "__main__":
     t0 = 0
-    n = 2
-    #ddt args
-    agents = ["agent_{}".format(i) for i in range(n)]
-    v0 = {'agent_0':np.array([5, 10, 0]), 'agent_1':np.array([2, 20, 0])}
-    a = {"agent_{}".format(i):np.array([0.0, -9.8, 0.0]) for i in range (0, n)}
-    dydt_args = (v0, a)
-
     t = 2
     h = 0.2
-    s = Solver(N = n)
-    s.solve(de.dydt_grav, dydt_args, t0, t, h)
+    n = 2
+
+    #ddt args
+    v0 = [np.array([5, 10, 0]), np.array([2, 20, 0])]
+    a = [np.array([0.0, -9.8, 0.0]) for i in range(0, n)]
+    w = np.zeros(3)
+    # maps agent -> variable -> arguments
+    dydt_args = [{'x':{'v0':v0[agent], 'a':a[agent]}, 'v':{'F':de.F, 'm':1.0}, 'L':{'T':de.T, 'w': w}} for agent in range(n)]
+
+    s = Solver(N = n, ddt_args = dydt_args)
+    s.solve(t0, t, h)
