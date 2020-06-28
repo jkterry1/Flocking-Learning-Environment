added more drag contributions to torque and fixed incorrect ydot calculation

RK4Solver/DiffEqs.py
@@ -9,6 +9,7 @@ def dudt(u, bird):
     return udot
 
 def dvdt(v, bird):
+    #g * sphi*cthe - r * ub + p * wb # = vdot
     Y = Fv(v, bird)/bird.m
     grav = bird.g * np.cos(bird.theta) * np.sin(bird.phi)
     vdot = Y + grav
@@ -35,13 +36,13 @@ def dxdt(x, bird):
             np.cos(psi)) * bird.w
 
 def dydt(y, bird):
+    #cthe*spsi * ub + (cphi*cpsi+sphi*sthe*spsi) * vb + (-sphi*cpsi+cphi*sthe*spsi) * wb # = yEdot
     theta = bird.theta
     phi = bird.phi
     psi = bird.psi
-    return np.cos(theta) * np.sin(psi) * bird.v + \
+    return np.cos(theta) * np.sin(psi) * bird.u + \
             (np.cos(phi) * np.cos(psi) + np.sin(phi) * np.sin(theta) * np.sin(psi)) * bird.v + \
-            (-np.sin(phi) * np.cos(psi) + np.cos(phi) * np.sin(theta) * \
-            np.sin(psi)) * bird.w
+            (-np.sin(phi) * np.cos(psi) + np.cos(phi) * np.sin(theta) * np.sin(psi)) * bird.w
 
 def dzdt(z, bird):
     theta = bird.theta
@@ -51,6 +52,7 @@ def dzdt(z, bird):
             bird.v + np.cos(phi) * np.cos(theta) * bird.w)
 
 def dpdt(p, bird):
+    #1/Ixx * (L + (Iyy - Izz) * q * r)  # = pdot
     Ix = bird.Ixx
     Iy = bird.Iyy
     Iz = bird.Izz
@@ -65,6 +67,7 @@ def dpdt(p, bird):
     return pdot
 
 def dqdt(q, bird):
+    #1/Iyy * (M + (Izz - Ixx) * p * r)  # = qdot
     Ix = bird.Ixx
     Iy = bird.Iyy
     Iz = bird.Izz
@@ -73,11 +76,12 @@ def dqdt(q, bird):
     r = bird.r
     M = TM(bird)
     qdot = (1.0/Iy)
-    qdot*= (M - r * p * (Ix - Iz) - Ixz * (p**2 - r**2))
+    qdot *= (M + r * p * (Iz - Ix) - Ixz * (p**2 - r**2))
     return qdot
 
 
 def drdt(r, bird):
+    #xdot[5] = 1/Izz * (N + (Ixx - Iyy) * p * q)  # = rdot
     Ix = bird.Ixx
     Iy = bird.Iyy
     Iz = bird.Izz
@@ -87,18 +91,21 @@ def drdt(r, bird):
     N = TN(bird)
     L = TL(bird)
     rdot = 1.0/(Iz - (Ixz**2 / Ix))
-    rdot *= (N + (Ixz / Ix) * (q * r * (Iz - Iy) - Ixz * p * q + L) -\
-            p * q * (Iy - Ix) - Ixz * q * r)
+    rdot *= (N + (Ixz / Ix) * (q * r * (Iz - Iy) - Ixz * p * q + L) +\
+            p * q * (Ix - Iy) - Ixz * q * r)
     return rdot
 
 def dthetadt(theta, bird):
+    #q * cphi - r * sphi  # = thetadot
     return bird.q * np.cos(bird.phi) - bird.r * np.sin(bird.phi)
 
 def dphidt(phi, bird):
+    #p + (q*sphi + r*cphi) * sthe / cthe  # = phidot
     return bird.p + (bird.q * np.sin(bird.phi) + \
             bird.r * np.cos(bird.phi)) * (np.sin(bird.theta)/np.cos(bird.theta))
 
 def dpsidt(psi, bird):
+    #(q * sphi + r * cphi) / cthe  # = psidot
     return (bird.q * np.sin(bird.phi) + \
             bird.r * np.cos(bird.phi)) * (1.0/np.cos(bird.theta))
 
@@ -132,8 +139,10 @@ def TM(bird):
     r = bird.Yl/4.0
     v = (bird.q * r)/2.0
     A = (bird.Xl * bird.Yl)
+    #rotation drag
     D = -np.sign(v) * r * bird.Cd * A * (bird.rho * v**2)/2.0
-    T += 2.0 * D + bird.vortex_torque_v
+    T += 2.0 * D
+    T += bird.vortex_torque_v
     bird.T[1] = T
     return T
 
@@ -143,20 +152,32 @@ def TN(bird):
     #lift
     A = bird.Xl * bird.Yl
     L = bird.Cl * A * (bird.rho * bird.u**2)/2.0
+
     #left wing lift in u direction
-    Fl = L * np.sin(bird.alpha_l)
+    Fl = - L * np.sin(bird.alpha_l)
+    #drag contribution from u direction motion
+    A = bird.Yl * bird.Xl * np.sin(bird.alpha_l)
+    Fl -= -np.sign(bird.u) * bird.Cl * A * (bird.rho * bird.u**2)/2.0
+
     #right wing lift in u direction
     Fr = L * np.sin(bird.alpha_r)
+    #drag contribution from u direction motion
+    A = bird.Yl * bird.Xl * np.sin(bird.alpha_r)
+    Fr + = -np.sign(bird.u) * bird.Cl * A * (bird.rho * bird.u**2)/2.0
 
-    T += -Fl * r
+    #add wing orientation contributions to the torque
+    T += Fl * r
     T += Fr * r
 
     v = (bird.r * r)/2.0
     A = (bird.Yl * bird.Zl)
-    #drag from rotation
-    D = -np.sign(v) * r * bird.Cd * A * (bird.rho * v**2)/2.0
-    T += 2.0 * D
+    #drag that slows rotation
+    D = -np.sign(v) * bird.Cd * A * (bird.rho * v**2)/2.0
+    T += 2.0 * D * r
+
+    #drag from vortices
     T += bird.vortex_torque_w
+
     bird.T[2] = T
     return T
 
@@ -164,16 +185,18 @@ def Fu(u, bird):
     F = 0
     #lift
     A = bird.Xl * bird.Yl
-    L = bird.Cl * A * (bird.rho * bird.u**2)/2.0
+    L = bird.Cl * A * (bird.rho * u**2)/2.0
     #Drag = Cd * (rho * v^2)/2 * A
-    A = bird.Xl * bird.Zl
+    A = bird.Xl * bird.Zl + bird.Xl * bird.Yl * np.cos(bird.alpha_l) + bird.Xl * bird.Yl * np.cos(bird.alpha_r)
     D = -np.sign(u) * bird.Cd * A * (bird.rho * u**2)/2.0
-    F += -L * np.sin(bird.alpha_l)
-    F += -L * np.sin(bird.alpha_r)
+    F += L * np.sin(bird.alpha_l)
+    F += L * np.sin(bird.alpha_r)
+    #print("Forward force from alpha ", L * np.sin(bird.alpha_r))
     F += D
     F += bird.vortex_force_u
+    F += bird.thrust
     bird.F[0] = F
-    return F + bird.thrust
+    return F
 
 def Fv(v, bird):
     F = 0
@@ -182,10 +205,10 @@ def Fv(v, bird):
     L = bird.Cl * A * (bird.rho * bird.u**2)/2.0
     #Drag = Cd * (rho * v^2)/2 * A
     A = bird.Yl * bird.Zl
-    D = np.sign(v) * bird.Cd * A * (bird.rho * v ** 2)/2.0
+    D = -np.sign(v) * bird.Cd * A * (bird.rho * v ** 2)/2.0
     F += L * np.sin(bird.beta_l)
     F += -L * np.sin(bird.beta_r)
-    F += -D
+    F += D
     F += bird.vortex_force_v
     bird.F[1] = F
     return F
@@ -194,17 +217,13 @@ def Fw(w, bird):
     F = 0
     #Lift = Cl * (rho * v^2)/2 * S
     #Drag = Cd * (rho * v^2)/2 * A
-    alpha_l = bird.alpha_l
-    alpha_r = bird.alpha_r
-    beta_l = bird.beta_l
-    beta_r = bird.beta_r
     A = bird.Xl * bird.Yl
     L = bird.Cl * A * (bird.rho * bird.u**2)/2.0
     D = -np.sign(w) * bird.Cd * A * (bird.rho * w ** 2)/2.0
     #left wing lift
-    F += L * abs(np.cos(alpha_l) * np.cos(beta_l))
+    F += L * np.cos(bird.alpha_l) * np.cos(bird.beta_l)
     #right wing lift
-    F += L * abs(np.cos(alpha_r) * np.cos(beta_r))
+    F += L * np.cos(bird.alpha_r) * np.cos(bird.beta_r)
     F += D
     F += bird.vortex_force_w
     bird.F[2] = F

RK4Solver/solver_tester.py
@@ -13,7 +13,7 @@ games = 1
 tik = time.time()
 
 birds = [Bird(z = 50.0, x = 1.0, u = 7.1271 , p = 0.0)]#, Bird(z = 5.0, y = .75, u = 5.0, p = 0.0),  Bird(z = 5.0, y = -.75, u = 5.0, p = 0.0)]
-birds = [Bird(z = 50.0, y = -3.0, u = 5.0, alpha_l = np.pi/4.0, alpha_r = np.pi/4.0)]#,Bird(z = 50.0, y = 3.0, u = 5.0)]
+birds = [Bird(z = 50.0, y = -3.0, u = 10.0, alpha_r = np.pi/4.0)]#,Bird(z = 50.0, y = 3.0, u = 5.0)]
 env = solver.raw_env(N = len(birds), h = h, birds = birds)
 env.reset()
 