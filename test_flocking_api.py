7f9108e932aff06dd19541f83fe0223a8af05a62
Justin Terry <justinkterry@gmail.com>
2020-12-30 22:29:39 -0500

    reduce number of playtests

--- test_flocking_api.py
+++ test_flocking_api.py
@@ -4,16 +4,16 @@ import pettingzoo.tests.performance_benchmark as performance_benchmark
 
 from pettingzoo.tests import render_test
 from pettingzoo.tests import error_test
-from pettingzoo.tests import seed_test 
+from pettingzoo.tests import seed_test
 from pettingzoo.tests import test_save_obs
-from pettingzoo.tests import max_cycles_test 
+from pettingzoo.tests import max_cycles_test
 
 import flocking_env
 
 
 env = flocking_env.env()
-api_test.api_test(env, num_cycles=1000, render=False, verbose_progress=True)
-seed_test.seed_test(env, num_cycles=1000)
+api_test.api_test(env, num_cycles=1, render=False, verbose_progress=True)
+seed_test.seed_test(env, num_cycles=1)
 performance_benchmark.performance_benchmark(env)
 
 # test render (both render test and api test w/ render)
