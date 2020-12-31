b168dab7af1a74bc66639775163e0f91704b5c75
Justin Terry <justinkterry@gmail.com>
2020-12-30 20:46:30 -0500

    rewrite tests

--- test_flocking_api.py
+++ test_flocking_api.py
@@ -1,13 +1,22 @@
-from pettingzoo.tests.api_test import api_test
-from pettingzoo.tests.seed_test import seed_test
+import pettingzoo.tests.api_test as api_test
+import pettingzoo.tests.bombardment_test as bombardment_test
+import pettingzoo.tests.performance_benchmark as performance_benchmark
+
+from pettingzoo.render_test import render_test as render_test
+from pettingzoo.error_tests import error_test as error_test
+from pettingzoo.seed_test import seed_test as seed_test
+from pettingzoo.save_obs_test import test_save_obs as test_save_obs
+from pettingzoo.max_cycles_test import max_cycles_test as max_cycles_test
+
 import flocking_env
 
 
 def test_flocking_env():
     env = flocking_env.env()
-    api_test(env, num_cycles=10000)
-    seed_test(flocking_env.env, num_cycles=10000)
+    api_test(env, num_cycles=1000, render=False, verbose_progress=True)
+    seed_test(env, num_cycles=1000)
+    performance_benchmark(env)
 
-
-if __name__ == "__main__":
-    test_flocking_env()
+# test render (both render test and api test w/ render)
+# test equivalent of save obs?
+# error tests
