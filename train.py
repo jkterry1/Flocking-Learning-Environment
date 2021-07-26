fdf98cc029329468aebe4a0921d936aab0354e88
Justin Terry <justinkterry@gmail.com>
2021-07-26 13:43:57 -0400

    skip frames fix

--- train.py
+++ train.py
@@ -19,18 +19,19 @@ n_timesteps = hz*60*n_agents*episodes
 print("n_timesteps: " + str(n_timesteps))
 distance_reward_per_m = 100/total_distance_m
 energy_reward_per_j = -10/total_energy_j
-print("skip_frames: " + str(hz/nerve_impulse_hz))
+skip_frames = int(hz/nerve_impulse_hz)
+print("skip_frames: " + str(skip_frames))
 
 env = flocking_env.parallel_env(N=n_agents, h=1/hz, energy_reward=energy_reward_per_j, forward_reward=distance_reward_per_m, crash_reward=crash_reward, LIA=True)
 env = ss.delay_observations_v0(env, reaction_frames)
-env = ss.frame_skip_v0(env, hz/nerve_impulse_hz)
+env = ss.frame_skip_v0(env, skip_frames)
 env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, n_envs, num_cpus=1, base_class='stable_baselines3')
 env = VecMonitor(env)
 
 eval_env = flocking_env.parallel_env(N=n_agents, h=1/hz, energy_reward=energy_reward_per_j, forward_reward=distance_reward_per_m, crash_reward=crash_reward, LIA=True)
 eval_env = ss.delay_observations_v0(eval_env, reaction_frames)
-eval_env = ss.frame_skip_v0(eval_env, hz/nerve_impulse_hz)
+eval_env = ss.frame_skip_v0(eval_env, skip_frames)
 eval_env = ss.pettingzoo_env_to_vec_env_v0(eval_env)
 eval_env = ss.concat_vec_envs_v0(eval_env, 1, num_cpus=1, base_class='stable_baselines3')
 eval_env = VecMonitor(eval_env)
