4f658c78df07edfeb781a545162b79477179d8cb
chorsch <22306897+chorsch@users.noreply.github.com>
2022-03-02 10:48:23 -0500

    Added updated unity project

---
+++ Unity Animation/Bird Rendering/Assets/Scripts/Automated_Rendering.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+//using UnityEditor.Recorder;
+//using UnityEditor.Recorder.Input;
+using FFmpegOut;
+
+public class Automated_Rendering : MonoBehaviour
+{
+    public String logs_folder;
+    List<Controller> controllers;
+    public GameObject camera;
+    public GameObject birdPrefab;
+    public int numBirds;
+    public bool record;
+    public bool in_editor;
+
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start(){
+      coroutine = Run();
+      StartCoroutine(coroutine);
+    }
+
+    private void StartRecorder(String f, int s)
+    {
+        string[] path = f.Split('/');
+
+        //calculate reward to determine what folder to put the video in
+        int reward = (int)(float.Parse(path[path.Length-1].Split('_')[2]))/10;
+        string folder = "";
+        folder = reward.ToString()+"0s";
+        Debug.Log("folder: "+folder);
+        string fileName = path[path.Length-1].Substring(0, path[path.Length-1].Length-4)+"-render";
+        fileName = f.Substring(0, f.Length-path[path.Length-1].Length-path[path.Length-2].Length-1) + folder + "-videos/" + fileName;
+
+    }
+
+
+    private IEnumerator Run()
+    {
+      DirectoryInfo dir = null;
+      if(!in_editor){
+        String[] path_split = Application.dataPath.Split('/');
+        String path = "";
+        for (int i=1; i<path_split.Length - 2; i++){
+          path = path + "/" + path_split[i];
+        }
+
+        dir = new DirectoryInfo(path + @"/" + logs_folder);
+      }
+      else{
+        dir = new DirectoryInfo(Application.dataPath + "/Scripts/"+ logs_folder);
+      }
+      FileInfo[] info = dir.GetFiles("*bird*.csv");
+
+      List<String> files = new List<String>();
+
+      foreach (FileInfo f in info)
+      {
+        files.Add(f.ToString());
+      }
+
+      CameraCapture camcap = camera.GetComponent<CameraCapture>();
+      files = files.Distinct().ToList();
+      foreach (String f in files)
+      {
+        //Debug.Log(f);
+        Controller controller = gameObject.AddComponent(typeof(Controller)) as Controller;
+        controller.numBirds = numBirds;
+        controller.bird_filename = f;
+        controller.camera = camera;
+        controller.birdPrefab = birdPrefab;
+
+        controller.Init_Animation();
+        if(record){
+          camcap.enabled = true;
+        }
+        //yield return new WaitForSeconds(4);
+        controller.Start_Animation();
+        int waitTime = controller.maxFrame/50 + 2;
+        yield return new WaitForSeconds(waitTime);
+        if(record){
+          camcap.enabled = false;
+        }
+      }
+      Application.Quit();
+
+    }
+
+
+}
---
+++ Unity Animation/Bird Rendering/Assets/Scripts/Controller.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Controller : MonoBehaviour{
+    public string bird_filename = "bird_log_1.csv";
+    public string vortex_filename = "vortex_log_1.csv";
+    public int numBirds;
+
+    public List<Vector3> path;
+    public List<Bird> birds;
+    List<Vortex> vortices = new List<Vortex>();
+    public int curFrame;
+    public int maxFrame;
+    public GameObject birdPrefab;
+    public GameObject vortexPrefab;
+    public GameObject camera;
+
+    int index;
+    public bool render_vortices = false;
+    float time = 0.0f;
+    List<Vortex> visible_vortices = new List<Vortex>();
+    int i=0;
+
+    public bool start;
+    public bool running = false;
+
+    //void Start(){}
+
+    public void Init_Animation(){
+        running = true;
+        curFrame = 0;
+        maxFrame = 0;
+        //string[] bird_lines = System.IO.File.ReadAllLines(Application.dataPath + @"/Scripts/" + bird_filename);
+        string[] bird_lines = System.IO.File.ReadAllLines(bird_filename);
+        //Debug.Log(bird_filename);
+        birds = new List<Bird>();
+
+        for (int i = 0; i < numBirds; i++){
+            //Bird temp = gameObject.AddComponent(typeof(Bird)) as Bird;
+            //temp.id = i;
+            Bird temp = new Bird(i);
+            birds.Add(temp);
+            //Debug.Log(birds.Count());
+            GameObject obj = Instantiate(birdPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            birds[birds.Count() - 1].AddModel(obj);
+        }
+
+        index = birds.Count()/2;
+        MoveCamera();
+
+        //int lines = 0;
+
+        foreach (string line in bird_lines){
+            //lines++;
+            string[] elements = line.Split(',');
+
+            int id = Int32.Parse(elements[0]);
+
+            float x = float.Parse(elements[3]);
+            float y = float.Parse(elements[4]);
+            float z = float.Parse(elements[2]);
+
+            float roll = float.Parse(elements[5]) * Mathf.Rad2Deg;
+            float pitch = float.Parse(elements[6]) * Mathf.Rad2Deg;
+            float yaw = float.Parse(elements[7]) * Mathf.Rad2Deg + 90.0f;
+
+            float alphaLeft = float.Parse(elements[8]) * Mathf.Rad2Deg;
+            float alphaRight = float.Parse(elements[9]) * Mathf.Rad2Deg;
+            float betaLeft = float.Parse(elements[10]) * Mathf.Rad2Deg;
+            float betaRight = float.Parse(elements[11]) * Mathf.Rad2Deg;
+
+            birds[id].AddState(new State(x, y, z, roll, pitch, yaw, alphaLeft, alphaRight, betaLeft, betaRight));
+            //Debug.Log(birds[id].states.Count());
+        }
+
+        foreach (Bird bird in birds){
+            if (bird.states.Count() >= maxFrame){
+                maxFrame = bird.states.Count();
+            }
+        }
+    }
+
+    public void Start_Animation(){
+        InvokeRepeating("StepBirds", 0.05f, 0.02f);
+        //InvokeRepeating("StepVortices", 2.0f, 0.1f);
+    }
+
+    void StepBirds()
+    {
+      foreach (Bird bird in birds){
+          bird.Step(curFrame);
+      }
+
+      curFrame++;
+      //curFrame = curFrame % maxFrame;
+      // Cancel all Invoke calls
+      if (curFrame >= maxFrame){
+        running = false;
+        camera.transform.parent = null;
+        RemoveBirds();
+        CancelInvoke();
+      }
+    }
+
+    void RemoveBirds(){
+      foreach(Bird bird in birds){
+        Destroy(bird.birdModel);
+      }
+    }
+
+    void StepVortices(){
+      List<Vortex> temp = new List<Vortex>();
+      while(vortices[i].time <= time){
+        temp.Add(vortices[i]);
+        vortices[i].spawn();
+        i++;
+      }
+      foreach (Vortex v in visible_vortices){
+        v.remove();
+        Destroy(v);
+      }
+      visible_vortices = temp;
+      time += 0.1f;
+    }
+
+
+    // Update is called once per frame
+    void Update(){
+      if(running){
+        MoveCamera();
+      }
+    }
+
+    void MoveCamera(){
+      camera.transform.position = new Vector3(birds[index].body.transform.position.x - 0.0f,
+                                              birds[index].body.transform.position.y + 60.0f,
+                                              birds[index].body.transform.position.z - 60.0f);
+      camera.transform.LookAt(birds[index].body.transform);
+    }
+}
