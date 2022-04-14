46ee9ce082da304e02516190a211b9d90909c38d
snow-fox <taijunjet@hotmail.com>
2022-04-14 15:49:05 +0100

    add mirror flip camera thing for linux

---
+++ Unity Animation/Bird Rendering/Assets/Scripts/MirrorFlipCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class MirrorFlipCamera : MonoBehaviour
+{
+    new Camera camera;
+    void Awake () {
+        camera = GetComponent<Camera>();
+    }
+
+    void OnPreCull() {
+        if (Application.platform == RuntimePlatform.LinuxPlayer) {
+            camera.ResetWorldToCameraMatrix();
+            camera.ResetProjectionMatrix();
+            Vector3 scale = new Vector3(-1, -1, 1);
+            camera.projectionMatrix = camera.projectionMatrix * Matrix4x4.Scale(scale);
+        }
+    }
+
+    void OnPreRender () {
+        if (Application.platform != RuntimePlatform.LinuxPlayer) return;
+        GL.invertCulling = false;
+    }
+
+    void OnPostRender () {
+        if (Application.platform != RuntimePlatform.LinuxPlayer) return;
+        GL.invertCulling = true;
+    }
+}
